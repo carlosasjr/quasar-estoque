6d491c81f3c0853594e7bcbaf2688c5851ba63ed
carlos <carlosasjr2003@hotmail.com>
2022-11-28 17:55:36 -0300

    me

--- src/pages/Me.vue
+++ src/pages/Me.vue
@@ -1,7 +1,7 @@
 <template>
   <q-page class="flex flex-center">
     <div v-if="user">
-      <p>Hello {{ user.email }}</p>
+      <p>Hello, {{ user.email }}</p>
     </div>
   </q-page>
 </template>
